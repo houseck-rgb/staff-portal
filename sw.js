2795518dce6764ea7eca2e4ab0a575b5dac6c52d
houseck-rgb <houseck@naver.com>
2026-06-14 22:47:14 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE = 'cwy-hub-v1';
-const ASSETS = ['./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon-dispatch.png','./icon-worker.png'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
